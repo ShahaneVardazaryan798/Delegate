6653dae08f23fad466972e68c0964d8d23aad020
ShahaneVardazaryan798 <58977401+ShahaneVardazaryan798@users.noreply.github.com>
2020-03-05 12:28:53 -0800

    222

---
+++ FirstDelegate/FirstDelegate/MyDelegate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDelegate
+{
+    public delegate void DelegateName(string name);
+    public delegate int DelegateAge(int age);
+    class MyDelegate
+    {
+        
+        public void GetName(string name)
+        {
+            Console.WriteLine(name);
+        }
+        public static int GetAge(int age)
+        {
+            return age;
+        }
+    
+    }
+}
---
+++ FirstDelegate/FirstDelegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace FirstDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate dlg = new MyDelegate();
+            dlg.GetName("Shahane");
+            //int age=MyDelegate.GetAge(24);
+            //WriteLine(age);
+            DelegateName dg = new DelegateName(dlg.GetName);
+            dg("Garik");
+            DelegateAge gd = new DelegateAge(MyDelegate.GetAge);
+            int age=gd(24);
+            WriteLine(age);
+            
+             
+        }
+    }
+}
